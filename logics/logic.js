bbe26899c04233830ba63faa2f2ebfe570f46992
Enrique Fernández <dpuentesf1@ucentral.edu.co>
2024-03-14 14:47:47 -0500

    Se agregaron funciones para dar un mejor control a la información que el usuario suministra en el fomulario. También, se realizaron arreglos a fallos de logia al evaluar si se envia o no la información del formulario

--- logics/logic.js
+++ logics/logic.js
@@ -1,15 +1,49 @@
+function checkFirstNameOrLastName(string){
+    const formato = /[!@#$%^&*()_+-=[\]{};':"\|,.<>/?]+/;
+    return !(formato.test(string));
+}
 
 function checkPasswordAndConfirPassword (PasswordText,ConfirPasswordText){
     let arrPass = Array.from(PasswordText);
     let arrConPass = Array.from(ConfirPasswordText);
-    let getComparison = PasswordText.length === ConfirPasswordText.length && arrPass.every((v,i)=>{ return v === arrConPass[i]});
-    console.log(getComparison);
+    if(PasswordText === "" || ConfirPasswordText === "") return false;
+    return PasswordText.length === ConfirPasswordText.length && arrPass.every((v,i)=>{ return v === arrConPass[i]});
 }
-const BotonEnviar = document.getElementById('BotonEnviar');
-BotonEnviar.addEventListener('click', ()=>{
-    const Email = document.getElementById('Email');
+
+function checkEmail (Email){
+    const formato = /[@]/;
+    if(!(formato.test(Email))) return false;
+    const secureEmailProviders = [ 'protonmail', 'tutanota', 'mailfence', 'startmail', 'zoho', 'hushmail', 'mailbox','hubspot','gmail','yahoo','outlook','fastmail','aol'];
+    let subCadena1 = Email.split("@");
+    let subCadena2 = subCadena1[1].split('.');
+    if(subCadena2[1] === 'edu'){
+        console.log('edu si funciona');
+        return true;
+    }else{
+        for(let i = 0; i < secureEmailProviders.length - 1; i++){
+            if(subCadena2[0] === secureEmailProviders[i]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const Form = document.getElementById('Form_Sign-up');
+Form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const firstName = document.getElementById('FirstName');
+    const lastname = document.getElementById('LastName');
+    const email = document.getElementById('Email');
     const Password = document.getElementById('Password');
     const ConfirPassword = document.getElementById('ConfirPassword');
-    let valuePass = checkPasswordAndConfirPassword (Password.value,ConfirPassword.value);
-    let valueEmail = checkPasswordAndConfirPassword ();
+    let valuePass = checkPasswordAndConfirPassword(Password.value,ConfirPassword.value);
+    let valueEmail = checkEmail(email.value);
+    let valueFisrtName = checkFirstNameOrLastName(firstName.value);
+    let valueLastName = checkFirstNameOrLastName(lastname.value);
+    if(valuePass === true && valueEmail === true && valueFisrtName === true && valueLastName === true){
+        Form.submit();
+    }else{
+        alert('No sale');
+    }
 });
